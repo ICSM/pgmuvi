33079097c6d575ff14b0cd3ed057b9cc10ecc039
Peter Scicluna <peter.scicluna@eso.org>
2021-07-19 03:26:13 -0400

    Adding placeholder setup.py and __init__.py

---
+++ pgmuvi/__init__.py
@@ -0,0 +1,7 @@
+""" pgmuvi: a package for infering multiwavelength variaiblity of astronomical sources using Gaussian processes in python
+
+"""
+
+
+__version__ = "0.0.1"
+__all__=[""]
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pgmuvi',
+    version=get_version("pgmuvi/__init__.py"),    
+    description='A package for GP inference of multiwavelength variability',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/icsm/pgmuvi',
+    project_urls={
+        "Bug Tracker": "https://github.com/icsm/pgmuvi/issues",
+    },
+    author='Peter Scicluna',
+    author_email='peter.scicluna@eso.org',
+    license='GPL',
+    packages=['pygmuvi'],
+    install_requires=['numpy',
+                      'matplotlib',
+                      'seaborn',
+                      'pytorch',
+                      'gpytorch',
+                      'pyro-ppl'
+                      ],
+    python_requires=">=3.7", 
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Public License v3.0',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
