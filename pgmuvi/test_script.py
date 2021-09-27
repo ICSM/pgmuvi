ca82aedf964c4aa1a7fb0d4519a01f1be6c4d4a2
Peter Scicluna <peter.scicluna@eso.org>
2021-09-26 22:34:30 -0300

    Doing some basic tests

--- pgmuvi/test_script.py
+++ pgmuvi/test_script.py
@@ -15,44 +15,92 @@ if __name__=="__main__":
     train_jd = torch.Tensor(testdata['JD'].to_numpy())
     train_mag = torch.Tensor(testdata['Magnitude'].to_numpy())
 
+
+    """ Let's generate some synthetic data from a perturbed sine curve 
+        but on the same time sampling as the real data"""
+
+    P = 137. #Days!
+    train_mag = torch.sin(train_jd*(2*np.pi/P))
+
+
+    train_mag = train_mag + 0.1*torch.randn_like(train_mag)
+
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
 
-    model = SMG(train_jd, train_mag, likelihood)
-    print(model)
-    print(model.covar_module)
+    model = SMG(train_jd, train_mag, likelihood, num_mixtures = 1)
+    #print(model)
+    #print(model.covar_module)
     #help(model)
+    #exit()
 
-    for name, param in model.named_parameters():
-        print(name, param)
+    #for name, param in model.named_parameters():
+    #    print(name, param)
     #[print(p) for p in model.parameters()]
+
+
+    #print(model.covar_module.raw_mixture_means,#.item(),
+    #      1/torch.exp(model.covar_module.raw_mixture_means),#.item(),
+    #      2*np.pi/torch.exp(model.covar_module.raw_mixture_means)#.item())
+    #      )
 
     #exit()
     model.train()
     likelihood.train()
 
-    training_iter = 100
+    #help(likelihood)
+
+    training_iter = 100#0
 
     
     # Use the adam optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
+    #help(optimizer)
     
     # "Loss" for GPs - the marginal log likelihood
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+    #help(mll)
+    #exit()
     
     for i in range(training_iter):
         optimizer.zero_grad()
         output = model(train_jd)
         loss = -mll(output, train_mag)
         loss.backward()
-        print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iter, loss.item()))
+        #print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iter, loss.item()))
         optimizer.step()
 
-    print(model)
+    #print(model)
 
-    for name, param in model.named_parameters():
-        print(name, param)
+    #for name, param in model.named_parameters():
+    #    print(name, param)
 
-    # Test points every 0.1 between 0 and 5
+    #print(model.covar_module.raw_mixture_means,#.item(),
+    #      1/torch.exp(model.covar_module.raw_mixture_means),#.item(),
+    #      2*np.pi/torch.exp(model.covar_module.raw_mixture_means)#.item())
+    #      )
+    #print(#model.covar_module.mixture_means,
+    #      1./model.covar_module.mixture_means,#.item(),
+    #      #2*np.pi/model.covar_module.mixture_means,
+    #      model.covar_module.mixture_weights)
+    #      #1/torch.exp(model.covar_module.raw_mixture_means),#.item(),
+    #      #2*np.pi/torch.exp(model.covar_module.raw_mixture_means)#.item())
+
+    if len(model.covar_module.mixture_means) == 1:
+        #For a single component mixture
+        print("Estimated mean magnitude: ", model.mean_module.constant.item())
+        print("Estimated frequency: ", model.covar_module.mixture_means.item()) #for a single component
+        print("Estimated width: ", model.covar_module.mixture_scales.item())
+        print("Fractional width: ", model.covar_module.mixture_scales.item()/model.covar_module.mixture_means.item())
+        print("Estimated period: ", 1./model.covar_module.mixture_means.item()) #for a single component
+        print("Estimated width: ", model.covar_module.mixture_scales.item()/model.covar_module.mixture_means.item() * 1./model.covar_module.mixture_means.item())
+        #print("Estimated width: ", 1./model.covar_module.mixture_scales.item())
+        
+        
+    
+        
+    #exit()
+    # 1000 test points across the range of the data
     test_x = torch.linspace(train_jd.min(), train_jd.max(), 1000)
 
     # Get into evaluation (predictive posterior) mode
--- pgmuvi/trainers.py
+++ pgmuvi/trainers.py
@@ -6,7 +6,60 @@ import pyro
 
 
 
-def train(model, likelihood, train_x, train_y, maxiter = 100, miniter = 10, stop = 0.01, loss='mll'):
+def train(model, likelihood, train_x, train_y,
+          maxiter = 100, miniter = 10, stop = None, lr = 1e-4,
+          lossfn='mll', optim = "SGD",
+          **kwargs):
+    ''' Given a GP model, a likelihood, and some training data, optimise a loss function to fit the training data.
+
+    Parameters
+    ----------
+    model : an instance of gpytorch.models.gp.GP or a subcluss thereof
+        The GP model whose (hyper-)parameters will be optimised.
+    likelihood : an instance of gpytorch.likelihoods.likelihood.Likelihood
+        The likelihood function for the Gaussian Process.
+    train_x : torch.Tensor or array-like
+        The values of the independent variables for training.
+    train_y : torch.Tensor or array-like
+        The values of the dependent variables for training.
+    maxiter : int, default 100
+        The maximum number of training iterations to use. If stop is not a positive number, this will be the number of iterations used to train.
+    miniter : int, default 10
+        The minimum number of training iterations to use. This parameter is only 
+        used if stop is a positive real number, in which case it is used to 
+        ensure that a sufficient number of iterations have been performed before
+        terminating training.
+    stop : float, default None
+        The fractional change in the loss function below which training will be 
+        terminated. If set to None, a negative value, not a number of a 
+        non-numerical type, training will continue until maxiter is reached.
+    lr : float, default 1e-4
+        The learning rate for the optimiser.
+    lossfn : string or instance of gpytorch.mlls.marginal_log_likelihood.MarginalLogLikelihood, default 'mll'
+        The loss function that will be used to evaluate the training. 
+        If a string, it must take one of the values 'mll' or 'elbo'.
+    optim : string or instance of torch.optim.optimizer.Optimizer, default 'SGD'
+        The optimizer that will be used to train the model.
+        If a string, it must take one of the values 'SGD', 'Adam', 'AdamW', 'NUTS'
+        Otherwise, it may be any torch or pyro optimiser. 
+
+    Examples
+    --------
+
+    '''
+    #Idea here is to provide a convenience function, so most of the time users only need to interact with this routine. It calls the other routines to do the training, which users can interact with if they choose to.
+    if isinstance(lossfn, str):
+        #Loss function is passed as a string, must be one of the values we understand:
+        if lossfn is 'mll':
+            #loss = -1* marginal log-likelihood
+            pass
+        elif lossfn is 'elbo':
+            #loss = -1* variational elbo, variational inference to be performed!
+            pass
+    elif isinstance(lossfn, ):
+        pass
+    else:
+        raise ValueError("lossfn must be either ")
     pass
 
 
