c0a4a74e92a95cb41cf16ae118ef57b502e393b8
Kathryn Jones <53044821+KathrynJones1@users.noreply.github.com>
2022-10-19 16:17:05 +0200

    Update setup.py
    Co-authored-by: Peter Scicluna <pscicluna@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Bug Tracker": "https://github.com/icsm/pgmuvi/issues",
     },
     author='Peter Scicluna, Kathryn Jones',
-    author_email='peter.scicluna@eso.org', 'kathryn.jones@unibe.ch'
+    author_email='peter.scicluna@eso.org, kathryn.jones@unibe.ch',
     license='GPL',
     packages=['pgmuvi'],
     install_requires=['numpy',
