b646d35bb07ef65f97e4503e1fc4246a71f8b41c
Peter Scicluna <pscicluna@users.noreply.github.com>
2023-07-21 19:07:57 -0400

    Forgot to add arviz and xarray to deps.
    Have to remember to convert this all to pyproject.toml soon!

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
                       'torch',
                       'gpytorch',
                       'pyro-ppl',
+                      'arviz',
+                      'xarray',
                       'tqdm'
                       ],
     python_requires=">=3.7", 
