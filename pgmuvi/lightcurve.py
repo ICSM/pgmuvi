72e1ce895a6a630f94203159a42aae7b9603103f
Sundar Srinivasan <s.srinivasan@irya.unam.mx>
2024-09-19 10:48:35 -0600

    Not casting `Path` object to `str`

--- pgmuvi/lightcurve.py
+++ pgmuvi/lightcurve.py
@@ -358,7 +358,7 @@ class Lightcurve(gpytorch.Module):
         from pathlib import Path
         from astropy.table import Table
         if isinstance(tab, str) or isinstance(tab, Path):
-            data = Table.read(str(tab), format=format)
+            data = Table.read(tab, format=format)
         elif isinstance(tab, astropy.table.Table):
             data = tab
         else:
